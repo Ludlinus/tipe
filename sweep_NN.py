40d11e7b4354195ca1495f2eba01c2db454a3a3c
DEGERMINY <arthauddegerminy@gmail.com>
2024-02-06 17:41:10 +0100

    Création sweep_4 et modification des hyper-paramètres

--- sweep_NN.py
+++ sweep_NN.py
@@ -1,16 +1,33 @@
 import wandb
+import run_NN
 
 wandb.login(key="1b1a372f1e56f15f8c0a2fd3cd93c5f5f5ca1dae")
 
 sweep_configuration = {
     "method": "bayes",
-    "name": "sweep_5",
+    "name": "sweep_4",
     "metric": {"goal": "maximize", "name": "fit_mean"},
     "parameters": {
-        "node_add_prob": {"max": 0.1, "min": 0.001},
-        "node_delete_prob": {"max": 0.05, "min": 0.001},
-        "survival_threshold": {"max": 0.40, "min": 0.05},
-        "pop_size": {"min": 450, "max": 550}
+        "node_add_prob": {"max": 0.01, "min": 0.001},
+        "node_delete_prob": {"max": 0.005, "min": 0.0005},
+        "survival_threshold": {"max": 0.30, "min": 0.10},
+        "pop_size": {"max": 750, "min": 250}
     },
 }
 sweep_id = wandb.sweep(sweep=sweep_configuration, project="TIPE-2", entity="sweat_pas_rose")
+
+
+#sweep_configuration = {
+#    "method": "bayes",
+#    "name": "sweep_3",
+ #   "metric": {"goal": "maximize", "name": "fit_mean"},
+  #  "parameters": {
+   #     "node_add_prob": {"max": 0.01, "min": 0.001},
+    #    "node_delete_prob": {"max": 0.005, "min": 0.0005},
+     #   "conn_add_prob": {"max": 0.5, "min": 0.05},
+      #  "conn_delete_prob": {"max": 0.25, "min": 0.025},
+       # "bias_max_value": {"max": 40, "min": 20},
+        #"weight_max_value": {"max": 100, "min": 10},
+        #"survival_threshold": {"max": 0.30, "min": 0.10}
+#    },
+#}
